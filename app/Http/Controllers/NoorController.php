0093b07ab9b78c2dfdf682c785beb0e6cc9b6cc3
NoorAleslam <manalaleslam@gmail.com>
2024-02-15 00:58:42 -0800

    first commit

---
+++ app/Http/Controllers/NoorController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class NoorController extends Controller
+{
+
+    //actions
+    public function inde()
+    {
+        //return response may is view.josn,file:
+        return view('about');
+    }
+    //public function student()
+    //{
+        //return response may is view.josn,file:
+      //  return view('welcome');
+    //}
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,155 @@
+<div>
+    <link href="./frame.css" rel="stylesheet" />
+    <div class="frame-container">
+      <div class="frame-frame">
+        <div class="frame-pajamasproject"></div>
+        <div class="frame-mdireceipttextsend"></div>
+        <img
+          alt="iconparkdownone2293"
+          src="/iconparkdownone2293-1ml.svg"
+          class="frame-iconparkdownone"
+        />
+        <img
+          alt="Rectangle712293"
+          src="/rectangle712293-qiye-1500h.png"
+          class="frame-rectangle71"
+        />
+        <div class="frame-component2">
+          <img alt="Line16I290" src="/line16i290-1cup.svg" class="frame-line16" />
+          <div class="frame-streamlinedefinitionsearchbooksolid"></div>
+          <img
+            alt="Rectangle186I290"
+            src="/rectangle186i290-xthd-200h.png"
+            class="frame-rectangle186"
+          />
+          <img alt="VectorI290" src="/vectori290-8wok.svg" class="frame-vector" />
+          <img
+            alt="VectorI290"
+            src="/vectori290-ywy.svg"
+            class="frame-vector01"
+          />
+        </div>
+        <div class="frame-component21">
+          <img
+            alt="Line16I290"
+            src="/line16i290-dyks.svg"
+            class="frame-line161"
+          />
+          <div class="frame-streamlinedefinitionsearchbooksolid1"></div>
+          <img
+            alt="Rectangle186I290"
+            src="/rectangle186i290-hzz-200h.png"
+            class="frame-rectangle1861"
+          />
+          <img
+            alt="VectorI290"
+            src="/vectori290-8s1.svg"
+            class="frame-vector02"
+          />
+          <img
+            alt="VectorI290"
+            src="/vectori290-3wgo.svg"
+            class="frame-vector03"
+          />
+        </div>
+        <div class="frame-component22">
+          <img
+            alt="Line16I290"
+            src="/line16i290-gm8s.svg"
+            class="frame-line162"
+          />
+          <div class="frame-streamlinedefinitionsearchbooksolid2"></div>
+          <img
+            alt="Rectangle186I290"
+            src="/rectangle186i290-pgci-200h.png"
+            class="frame-rectangle1862"
+          />
+          <img
+            alt="VectorI290"
+            src="/vectori290-yqdp.svg"
+            class="frame-vector04"
+          />
+          <img
+            alt="VectorI290"
+            src="/vectori290-pl57.svg"
+            class="frame-vector05"
+          />
+        </div>
+        <div class="frame-component23">
+          <img
+            alt="Line16I290"
+            src="/line16i290-xu3j.svg"
+            class="frame-line163"
+          />
+          <div class="frame-streamlinedefinitionsearchbooksolid3"></div>
+          <img
+            alt="Rectangle186I290"
+            src="/rectangle186i290-jnkm-200h.png"
+            class="frame-rectangle1863"
+          />
+          <img
+            alt="VectorI290"
+            src="/vectori290-6z0l.svg"
+            class="frame-vector06"
+          />
+          <img
+            alt="VectorI290"
+            src="/vectori290-tnc9.svg"
+            class="frame-vector07"
+          />
+        </div>
+        <div class="frame-component24">
+          <img
+            alt="Line16I290"
+            src="/line16i290-603j.svg"
+            class="frame-line164"
+          />
+          <div class="frame-streamlinedefinitionsearchbooksolid4"></div>
+          <img
+            alt="Rectangle186I290"
+            src="/rectangle186i290-9xu-200h.png"
+            class="frame-rectangle1864"
+          />
+          <img
+            alt="VectorI290"
+            src="/vectori290-i6s.svg"
+            class="frame-vector08"
+          />
+          <img
+            alt="VectorI290"
+            src="/vectori290-l8dq.svg"
+            class="frame-vector09"
+          />
+        </div>
+        <div class="frame-component25">
+          <img alt="Line16I290" src="/line16i290-sbo.svg" class="frame-line165" />
+          <div class="frame-streamlinedefinitionsearchbooksolid5"></div>
+          <img
+            alt="Rectangle186I290"
+            src="/rectangle186i290-sti-200h.png"
+            class="frame-rectangle1865"
+          />
+          <img
+            alt="VectorI290"
+            src="/vectori290-yas.svg"
+            class="frame-vector10"
+          />
+          <img
+            alt="VectorI290"
+            src="/vectori290-o89e.svg"
+            class="frame-vector11"
+          />
+        </div>
+        <span class="frame-text"><span>عنوان المشروع</span></span>
+        <span class="frame-text2"><span>القسم</span></span>
+        <span class="frame-text4"><span>فصل التخرج</span></span>
+        <span class="frame-text6"><span>اسم الطالب</span></span>
+        <img alt="back2907" src="/back2907-i6bm.svg" class="frame-back" />
+        <img
+          alt="Rectangle1873011"
+          src="/rectangle1873011-adcn-200h.png"
+          class="frame-rectangle187"
+        />
+      </div>
+    </div>
+  </div>
